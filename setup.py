f826820e4547aa6b682b8e152634bb1d12d3d5bb
haih <k7268wre>
2019-06-28 12:43:59 +0300

    add setup file

---
+++ setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="time_tensor",
+    version="0.1",
+    author="Hai Victor Habi",
+    license="MIT",
+    packages=find_packages(exclude=['tests', 'example'])
+)
+
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="torchrain",
+    version="0.0.1",
+    author="Hai Victor Habi",
+    author_email="victoropensource@gmail.com",
+    description="TorchRain package",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/haihabi/torch_rain",
+    packages=setuptools.find_packages(exclude=['tests', 'scripts']),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
