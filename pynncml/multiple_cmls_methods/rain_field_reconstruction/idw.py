f2fc563e828ffa548206b059a2fcb9ca98fa6d24
Hai Victor Habi <haivictorh@mail.tau.ac.il>
2024-07-04 21:29:40 +0300

    Multiple python version tests

--- pynncml/multiple_cmls_methods/rain_field_reconstruction/idw.py
+++ pynncml/multiple_cmls_methods/rain_field_reconstruction/idw.py
@@ -2,12 +2,20 @@ import torch
 from torch import nn
 import numpy as np
 from torch.nn import Parameter
-
 from pynncml.datasets import LinkSet
 
 
 class InverseDistanceWeighting(nn.Module):
     def __init__(self, in_h, in_w, modified=True, r=4, point_set=None, eps=1e-6):
+        """
+        Inverse Distance Weighting module for rain field reconstruction.
+        :param in_h: Height of the input grid
+        :param in_w: Width of the input grid
+        :param modified: Use modified IDW or not
+        :param r: Radius of influence
+        :param point_set: Set of points to be used for IDW
+        :param eps: Epsilon value to avoid division by zero
+        """
         super(InverseDistanceWeighting, self).__init__()
         y_grid_vector = np.linspace(0, 1, in_h)
         x_grid_vector = np.linspace(0, 1, in_w)
@@ -23,10 +31,15 @@ class InverseDistanceWeighting(nn.Module):
         self.r = r
         self.eps = eps
         self.point_set = point_set
-        if self.point_set is not None:
+        if point_set is not None:
             self.point_set2weight()
+        self.w = None
 
     def point_set2weight(self):
+        """
+        Calculate the weights for the points in the point set
+
+        """
         x = self.point_set
         # rain_est in shape [N Sensors,1,N_Step] and link_set
         d = self._calculate_distance(x.unsqueeze(dim=0))
@@ -40,6 +53,12 @@ class InverseDistanceWeighting(nn.Module):
         self.w[is_zero] = torch.max(self.w)  # set zero distance to max weight
 
     def forward(self, rain_est, link_set: LinkSet):
+        """
+        Infer rain fields from a set of links.
+        :param rain_est: Rain estimation tensor
+        :param link_set: Set of links
+        :return: Rain fields
+        """
         if self.point_set is None:
             locations = link_set.center_point(scale=True)
             self.point_set = torch.tensor(locations, device=rain_est.device).float()
@@ -50,5 +69,6 @@ class InverseDistanceWeighting(nn.Module):
         return rain_map_non_zero  # add channel axis
 
     def _calculate_distance(self, x_i):
+
         x_i = x_i.unsqueeze(dim=-1).unsqueeze(dim=-1)
         return torch.sqrt(torch.pow(x_i - self.grid, 2.0).sum(dim=2))
