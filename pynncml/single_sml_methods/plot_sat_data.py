7862bddf9d51dffa97e8120407f0140955c681c9
haivictorh <haivictorh@mail.tau.ac.il>
2025-06-16 22:34:29 +0300

    Change to torch

--- pynncml/single_sml_methods/plot_sat_data.py
+++ pynncml/single_sml_methods/plot_sat_data.py
@@ -26,37 +26,88 @@ def plot_rain_vs_sat_data(rain_array: torch.Tensor, rain_timestamp, sat_array: t
 import numpy as np
 
 
-def PlotRainEstimationVsGaugeData(RainEst, RainDF, SatDF, ST, FT):
+# def PlotRainEstimationVsGaugeData(RainEst, RainDF, SatDF, ST, FT):
+def plot_rain_estimation_vs_gauge_data(rain_estimation: torch.Tensor,
+                                       ST: torch.Tensor,
+                                       FT: torch.Tensor,
+                                       rain_array: torch.Tensor,
+                                       rain_timestamp,
+                                       sat_array: torch.Tensor,
+                                       sat_timestamp):
+    """
+    Plots the rain estimation against the rain gauge data and satellite signal data.
+    :param rain_estimation: 2D tensor of shape [n_samples, timesteps] containing the rain estimation data.
+    :param ST: 2D tensor of shape [n_samples, timesteps] containing the SlowTracker data.
+    :param FT: 2D tensor of shape [n_samples, timesteps] containing the FastTracker data.
+    :param rain_array: 2D tensor of shape [n_samples, timesteps] containing the rain gauge data.
+    :param rain_timestamp: Timestamps corresponding to the rain gauge data.
+    :param sat_array: 2D tensor of shape [n_samples, timesteps] containing the satellite signal data.
+    :param sat_timestamp: Timestamps corresponding to the satellite signal data.
+    :return: None
+
+    """
+
+    if len(rain_array.shape) != 2:
+        raise ValueError("rain_array should be a 2D tensor with shape [n_samples, timesteps]")
+    if rain_array.shape[1] < 2:
+        raise ValueError("rain_array should have at least 2 timesteps")
+    if rain_array.shape[0] < 1:
+        raise ValueError("rain_array should have at least 1 sample")
+    if rain_array.dtype != torch.float32 and rain_array.dtype != torch.float64:
+        raise TypeError("rain_array must be of type torch.float32 or torch.float64")
+
+    if len(sat_array.shape) != 2:
+        raise ValueError("sat_array should be a 2D tensor with shape [n_samples, timesteps]")
+    if sat_array.shape[1] < 2:
+        raise ValueError("sat_array should have at least 2 timesteps")
+    if sat_array.shape[0] < 1:
+        raise ValueError("sat_array should have at least 1 sample")
+    if sat_array.dtype != torch.float32 and sat_array.dtype != torch.float64:
+        raise TypeError("sat_array must be of type torch.float32 or torch.float64")
+
+    if len(rain_estimation.shape) != 2:
+        raise ValueError("rain_estimation should be a 2D tensor with shape [n_samples, timesteps]")
+    if rain_estimation.shape[1] < 2:
+        raise ValueError("rain_estimation should have at least 2 timesteps")
+    if rain_estimation.shape[0] < 1:
+        raise ValueError("rain_estimation should have at least 1 sample")
+    if rain_estimation.dtype != torch.float32 and rain_estimation.dtype != torch.float64:
+        raise TypeError("rain_estimation must be of type torch.float32 or torch.float64")
+
     fig, ax = plt.subplots(1, figsize=(15, 10))
     ax1 = ax.twinx()
 
-    ax.plot_date(SatDF.timestamp, SatDF.esno, '-', color='purple', label='Esno Data ')
-    ax.plot_date(SatDF.timestamp, ST, '-', color='c', label='SlowTracker')
-    ax.plot_date(SatDF.timestamp, FT, '-', color='b', label='FastTracker')
-    ax1.plot_date(SatDF.timestamp, RainEst, '-', color='r', label='Rain Rate')
-    ax1.plot_date(SatDF.timestamp, np.cumsum(RainEst / 120), '-', color='k', label='Cumulative Sum Rain Rate')
+    rain_array_np = rain_array.cpu().detach().numpy()[0, :].flatten()
+    sat_array_np = sat_array.cpu().detach().numpy()[0, :].flatten()
+    rain_est = rain_estimation.cpu().detach().numpy()[0, :].flatten()
+    ST = ST.cpu().detach().numpy()[0, :].flatten()
+    FT = FT.cpu().detach().numpy()[0, :].flatten()
 
-    ax1.plot_date(RainDF.datetime, RainDF.value, '-', color='g', label="Rain Gauge [HaKfar HaYarok]")
-    ax1.plot_date(RainDF.datetime, np.cumsum(RainDF.value / 12), '-', color='y',
-                  label="Cumulative Rain Gauge [HaKfar HaYarok]")
-    RainGaugSum = np.sum(RainDF.value / 12)
-    EstRainSum = np.sum(RainEst / 120)
+    ax.plot_date(sat_timestamp, sat_array_np, '-', color='purple', label='Esno Data ')
+    ax.plot_date(sat_timestamp, ST, '-', color='c', label='SlowTracker')
+    ax.plot_date(sat_timestamp, FT, '-', color='b', label='FastTracker')
+    ax1.plot_date(sat_timestamp, rain_est, '-', color='r', label='Rain Rate')
+    ax1.plot_date(sat_timestamp, np.cumsum(rain_est/120), '-', color='k', label='Cumulative Sum Rain Rate')
+
+    ax1.plot_date(rain_timestamp, rain_array_np, '-', color='g', label="Rain Gauge")
+    ax1.plot_date(rain_timestamp, np.cumsum(rain_array_np / 12), '-', color='y',
+                  label="Cumulative Rain Gauge")
+    RainGaugSum = np.sum(rain_array_np / 12)
+    EstRainSum = np.sum(rain_est / 120)
 
     ax.xaxis.set_major_formatter(mdates.DateFormatter('%Y-%m-%d %H:%M'))
     # ax1.set_ylim([0, 10])
-    ax.set_ylim([-15, 12])
+    # ax.set_ylim([-15, 12])
     ax.set_ylabel('Satellite  Signal Es/No [dB]')
 
-    ax1.set_ylim([0, 80])
+    # ax1.set_ylim([0, 80])
     ax1.set_ylabel('Rain[mm/h]')
 
     ax.legend(loc='upper left')
     ax1.legend(loc='upper right')
     ax.grid()
 
-    fig.suptitle('satellite link measurements at ' + str(
-        RainDF.datetime.iloc[100].date()) + "\n \n" + "Sum HAKFAR HAYAROK   " + str(RainGaugSum) + "   [mm]" +
-
+    fig.suptitle('satellite link measurements with rain rate of' + str(RainGaugSum) + "   [mm]" +
                  "      |        Sum Algorithm   " + str(EstRainSum) + "   [mm]" + " \n")
     # fig.suptitle(' Rain Gauge Vs Sat Data at Kfar Saba ' + str(RainDF.datetime.iloc[100].date()))
     fig.tight_layout()
--- pynncml/single_sml_methods/sml_kalman_rain_estimation.py
+++ pynncml/single_sml_methods/sml_kalman_rain_estimation.py
@@ -1,5 +1,6 @@
 import numpy as np
-
+import torch
+import math
 from pynncml.single_sml_methods.kalman_filter import apply_kalman_filter_ft, apply_kalman_filter_st
 
 DiffGapValue = 0.4
@@ -11,74 +12,113 @@ Tr = 14
 alpha = 28.14  # 17.910
 beta = 0.798
 teta = 31.58 * np.pi / 180
+teta_sin = float(np.sin(teta))
 dh = 0.400
 h0 = 2
 
-def sml_rain_estimator(sat_data, preprocess=False):
-    din_r = sat_data.esno.to_numpy()
+
+def sml_rain_estimator(sat_data: torch.Tensor, preprocess=False):
+    """
+    Estimates rain rate from satellite data using Kalman filtering and power law.
+    :param sat_data: A 2D tensor of shape (n_samples, timesteps) containing satellite data.
+    :param preprocess: If True, applies preprocessing to the satellite data.
+    :return: A tuple containing the estimated rain rate, ST, and FT.
+    """
+    if not isinstance(sat_data, torch.Tensor):
+        raise TypeError("sat_data must be a torch.Tensor")
+    if len(sat_data.shape) != 2:
+        raise ValueError("sat_data must be a 2D tensor with shape (n_samples, timesteps)")
+    if sat_data.shape[1] < 2:
+        raise ValueError("sat_data must have at least 2 timesteps")
+    if sat_data.shape[0] < 1:
+        raise ValueError("sat_data must have at least 1 sample")
+    if sat_data.dtype != torch.float32 and sat_data.dtype != torch.float64:
+        raise TypeError("sat_data must be of type torch.float32 or torch.float64")
+
+    din_r = sat_data  # sat_data.esno.to_numpy()
     if preprocess:
         din_r = sat_data_preprocessing(din_r)
-
     ST = apply_kalman_filter_st(din_r)
     FT = apply_kalman_filter_ft(din_r)
 
     [FT, ST, Diff_Rain_Flag] = apply_st_mask(FT, ST)
-    L = Calc_L(FT, ST)
-    ####################  Apply power Law  ##############################################
-    RainRate_org = ApplyPowerLaw(L)
-    RainEstimated= ApplyDiffMask(RainRate_org, Diff_Rain_Flag)
+    L = calc_l(FT, ST)
 
+    ####################  Apply power Law  ##############################################
+    RainRate_org = apply_power_law(L)
+    RainEstimated = ApplyDiffMask(RainRate_org, Diff_Rain_Flag)
     return RainEstimated, ST, FT
 
 
-def apply_st_mask(FT, ST):
-    Diff = ST - FT
+def apply_st_mask(FT: torch.Tensor, ST: torch.Tensor):
+    delta = ST - FT
 
-    Diff_Rain_Flag = np.zeros((len(Diff)))
-    Diff_Rain_Flag[Diff >= DiffGapValue] = 1
-    z2one_ind = np.where(np.roll(Diff_Rain_Flag, 1) != Diff_Rain_Flag)[0]
+    diff_rain_ind = torch.zeros(delta.shape)
+    diff_rain_ind[delta >= DiffGapValue] = 1
+    sample, z2one_ind = torch.where(torch.roll(diff_rain_ind, 1, dims=1) != diff_rain_ind)
+    for s in torch.unique(sample):
+        _z2one_ind= z2one_ind[sample == s]
+        _z2one_ind = _z2one_ind[1:len(_z2one_ind) - 1]
+        z2onemat = torch.reshape(_z2one_ind, (-1, 2))
+        for ind in z2onemat:
+            ST[s, ind[0]:ind[1]] = ST[s, ind[0] + 1]
 
-    z2one_ind = z2one_ind[1:len(z2one_ind) - 1]
-    z2onemat = np.reshape(z2one_ind, (-1, 2))
-
-    for ind in z2onemat:
-        ST[ind[0]:ind[1]] = ST[ind[0] + 1]
-
-    return FT, ST, Diff_Rain_Flag
+    return FT, ST, diff_rain_ind
 
 
-def Calc_L(FT, ST):
-    L = (np.multiply(np.divide(FT, ST), (Tc / La + Tm * (1 - 1 / La) + Tg + Tr)) + (Tm - Tc) / La) / (Tm + Tg + Tr)
+def calc_l(FT: torch.Tensor, ST: torch.Tensor):
+    L = (torch.multiply(torch.divide(FT, ST), (Tc / La + Tm * (1 - 1 / La) + Tg + Tr)) + (Tm - Tc) / La) / (
+            Tm + Tg + Tr)
     return L
 
 
-def sat_data_preprocessing(in_data, timedelta=0.5, interval=15):
-    indexes = np.ceil(interval / timedelta)
-    Din_r = np.zeros(len(in_data))
-    for x in range(in_data.shape[0]):
-        if x > 0:
-            if x <= indexes:
-                Din_r[x] = -1 * (np.max(in_data[0:x]) - in_data[x])
+# def sat_data_preprocessing(in_data, timedelta=0.5, interval=15):
+#     indexes = np.ceil(interval / timedelta)
+#     Din_r = np.zeros(len(in_data))
+#     for x in range(in_data.shape[0]):
+#         if x > 0:
+#             if x <= indexes:
+#                 Din_r[x] = -1 * (np.max(in_data[0:x]) - in_data[x])
+#             else:
+#                 Din_r[x] = -1 * (np.max(in_data[x - int(indexes):x]) - in_data[x])
+#         else:
+#             Din_r[x] = in_data[x]
+#     Din_r = Din_r + np.median(in_data)
+#     return Din_r
+
+def sat_data_preprocessing(in_data: torch.Tensor, timedelta=0.5, interval=15):
+    indexes = math.ceil(interval / timedelta)
+    din_r = torch.zeros(in_data.shape)
+    for ti in range(in_data.shape[1]):
+        if ti > 0:
+            if ti <= indexes:
+                din_r[:, ti] = -1 * (torch.max(in_data[:, 0:ti], dim=1)[0] - in_data[:, ti])
             else:
-                Din_r[x] = -1 * (np.max(in_data[x - int(indexes):x]) - in_data[x])
+                din_r[:, ti] = -1 * (torch.max(in_data[:, ti - int(indexes):ti], dim=1)[0] - in_data[:, ti])
         else:
-            Din_r[x] = in_data[x]
-    Din_r = Din_r + np.median(in_data)
-    return Din_r
+            din_r[:, ti] = in_data[:, ti]
+    din_r = din_r + torch.median(in_data, dim=1).values
+    return din_r
 
 
 def ApplyDiffMask(RainRate_org, Diff_Rain_Flag):
-    RainRate = np.multiply(RainRate_org, Diff_Rain_Flag)
-    RainEstimated = RainRate[~np.isnan(np.nan_to_num(RainRate,
-                                                     0))]  # 120 - normelize rain rate to mm/h ( we have 120 samples of 30 second in one hour  )
+    rain_rate = torch.multiply(RainRate_org, Diff_Rain_Flag)
 
-    return RainEstimated
+    rain_estimated = torch.stack(
+        [rain_rate[i, ~torch.isnan(torch.nan_to_num(rain_rate[i,:], 0))] for i in range(rain_rate.shape[0])],
+        dim=0)  # 120 - normelize rain rate to mm/h ( we have 120 samples of 30 second in one hour  )
+    return rain_estimated
 
 
-def ApplyPowerLaw(L):
-    L_dB = 10 * np.log10(L)
+def apply_power_law(L: torch.Tensor):
+    # L_dB = 10 * np.log10(L)
+    L_dB = 10 * torch.log10(L)
     hr = h0 - dh
-    L1_dB = L_dB * np.sin(teta) / hr
-    RainRate_org = alpha * (np.power(L1_dB, beta, dtype=complex))
-    RainRate_org = abs(RainRate_org)
-    return (RainRate_org)
+    # L1_dB = L_dB * np.sin(teta) / hr
+    L1_dB = L_dB * teta_sin / hr
+    # RainRate_org = alpha * (np.power(L1_dB, beta, dtype=complex))
+    L1_dB_complex=torch.complex(L1_dB,torch.zeros_like(L1_dB))
+    RainRate_org = alpha * (torch.pow(L1_dB_complex, beta))
+    # RainRate_org = abs(RainRate_org)
+    RainRate_org = torch.abs(RainRate_org)
+    return RainRate_org
